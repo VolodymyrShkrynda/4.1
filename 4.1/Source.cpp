313e42081363722883fae69106d2b12802788897
VolodymyrShcrynda <volodymyr.shkrynda.itis.2020@lpnu.ua>
2020-10-26 14:49:10 +0200

    first master commit

---
+++ 4.1/Source.cpp
@@ -0,0 +1,43 @@
+// Lab_03_4.cpp
+// < Шкринда Володимир
+// Лабораторна робота № 4.1
+// Цикли.
+// Варіант 22
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int i, N;
+	double S;
+	cout << "N = "; cin >> N;
+	S = 0;
+	i = N;
+	while (i <= 20)
+	{
+		S += ((cos(i) + sin(i)) / 1 + cos(i) * sin(i));
+		i++;
+	}
+	cout << S << endl;
+	S = 0;
+	i = N;
+	do {
+		S += ((cos(i) + sin(i)) / 1 + cos(i) * sin(i));
+		i++;
+	} while (i <= 20);
+	cout << S << endl;
+	S = 0;
+	for (i = N; i <= 20; i++)
+	{
+		S += ((cos(i) + sin(i)) / 1 + cos(i) * sin(i));
+	}
+	cout << S << endl;
+	S = 0;
+	for (i = 20; i >= N; i--)
+	{
+		S += ((cos(i) + sin(i)) / 1 + cos(i) * sin(i));
+	}
+	cout << S << endl;
+	
+	cin.get();
+	return 0;
+}
